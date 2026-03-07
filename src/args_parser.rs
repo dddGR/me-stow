6b7b333ecc684d7868ce38af197a9d5f2ae3f849
dddGR <dddmake.zero@gmail.com>
2026-03-07 16:20:37 +0700

    add -- basic cli args parser

---
+++ src/args_parser.rs
@@ -0,0 +1,59 @@
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Command,
+}
+
+impl Cli {
+    pub fn new() -> Self {
+        Cli::parse()
+    }
+}
+
+#[derive(Subcommand)]
+pub enum Command {
+    /// Sync packages in source with system
+    Sync {
+        /// Name of packages to deploy, omit will deploy all
+        packages: Option<Vec<String>>,
+
+        /// Check for differences only
+        #[arg(short, long, action = clap::ArgAction::SetTrue)]
+        diff: bool,
+
+        /// Override current file on system if conflicts happen.
+        #[arg(short, long, action = clap::ArgAction::SetTrue)]
+        force: bool,
+    },
+    /// Stow files to package
+    Stow {
+        /// Name of package to stow
+        package: String,
+
+        /// Files to stow to that package
+        files: Vec<PathBuf>,
+    },
+    /// Remove selected packages
+    Remove {
+        /// Name of packages to remove
+        packages: Vec<String>,
+
+        /// Copy file actual file back to the system
+        #[arg(short, long, action = clap::ArgAction::SetTrue)]
+        copyback: bool,
+    },
+
+    /// List all current packages
+    List {
+        /// Name of package to list, omit will list all
+        package: Option<String>,
+
+        /// List all the files include in packages
+        #[arg(short, long, action = clap::ArgAction::SetTrue)]
+        all: bool,
+    },
+}
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,39 @@
 #![allow(unused_variables)]
 #![allow(dead_code)]
 
-fn main() {
-    println!("WELLCOME TO '\x1b[32mMe Stow\x1b[0m'");
-}
+mod args_parser;
 
+use args_parser::{Cli, Command};
+
+fn main() {
+    let cli = Cli::new();
+    println!("Running 'me-stow'");
+
+    match &cli.command {
+        Command::Sync {
+            packages: package,
+            diff,
+            force,
+        } => {
+            println!(
+                "Sync '{:?}' with [{}] and force: {}",
+                package, diff, force
+            )
+        }
+        Command::Stow { package, files } => {
+            println!(
+                "Stowing files: {:?} to package: '{}'",
+                files, package
+            )
+        }
+        Command::Remove { packages, copyback } => {
+            println!(
+                "Removing package: {:?} with copyback: '{}'",
+                packages, copyback
+            )
+        }
+        Command::List { .. } => {
+            println!("List packages")
+        }
+    }
+}
